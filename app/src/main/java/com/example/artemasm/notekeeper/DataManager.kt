fc569f5b3cbf3cddda6e203d200120a875a08e04
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-24 22:00:25 +0000

    Add Datamanager for NoteKeeper

--- app/src/main/java/com/example/artemasm/notekeeper/DataManager.kt
+++ app/src/main/java/com/example/artemasm/notekeeper/DataManager.kt
@@ -3,4 +3,22 @@ package com.example.artemasm.notekeeper
 class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    init {
+        initialiseCourses()
+    }
+
+    private fun initialiseCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "The Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
 }
